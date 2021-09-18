5e9375ed41e0caea01a4a470d3bf369c4038d756
NazarServer322 <66203891+NazarServer322@users.noreply.github.com>
2021-09-18 10:05:38 +0300

    Create scritps.sh

---
+++ scritps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum -y update
+sudo yum update -y
+sudo amazon-linux-extras install docker -y 
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+ sudo docker pull nazarserver/shoptest
+ sudo docker run -d --restart=always -p 80:80 --name market nazarserver/shoptest
